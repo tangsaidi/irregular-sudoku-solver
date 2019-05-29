be6ef609c0f026fd449a22b98f841f5412309ed5
tangsaidi <tangsaidi@icloud.com>
2019-05-29 16:34:01 -0400

    Added more comments

--- IrregularSudoku.h
+++ IrregularSudoku.h
@@ -5,13 +5,18 @@
 
 class IrregularSudoku {
     private:
+        // size of board
         int n;
-
+        // boardByRegion[i][j] stores j-th cell of region i
         std::vector<std::vector<int>> boardByRegion;
+        // boardByRow[i][j] stores which (region, cell) does i-th row, j-th column corresponds to
         std::vector<std::vector<std::pair<int, int>>> boardByRow;
+        // regionToRowCol[i][j] is the inverse mapping of boardByRow, this is helps us map between region, row and column
         std::vector<std::vector<std::pair<int, int>>> regionToRowCol;
+        // filled[i][j] == 1 means region i's j-th cell is prefilled
         std::vector<std::vector<bool>> filled;
 
+        // helper functions
         int readInput();
         bool checkValidInRegion(int, std::vector<int>&);
         bool checkValidInRowCol(int, int, int, std::vector<std::vector<int>> &,
@@ -32,6 +37,7 @@ class IrregularSudoku {
         friend std::ostream& operator<<(std::ostream &, const IrregularSudoku &);
 };
 
+// Overloading << operator for IrregularSudoku
 std::ostream& operator<<(std::ostream &, const IrregularSudoku &);
 
 #endif
--- example.cpp
+++ example.cpp
@@ -13,20 +13,24 @@ int main(int argc, char *argv[]) {
             silent = true;    
         }
     }
-
     IrregularSudoku iso = IrregularSudoku(silent);
+    
+    // Prints out the Sudoku object
     std::cout << iso;
 
+    // Copy the object 20 times, which will be used for benchmarking purposes later
     int repeats = 20;
     std::vector<IrregularSudoku> isoArr(repeats, IrregularSudoku(iso));
     std::cout << "=====================" << std::endl;
     clock_t tStart = clock();
     
+    // if the run was successful print out the board, else notify user
     if (iso.runDfs(0, 0) == 0) {
         std::cout << "Time taken: " << (double)(clock() - tStart)/(CLOCKS_PER_SEC) << "s" << std::endl;
         std::cout << "Solved Board" << std::endl;
         std::cout << iso;
         double sum = 0;
+        // Run dfs 20 times and print the results
         for (int i = 0; i < repeats; i++) {
             tStart = clock();
             isoArr[i].runDfs(0, 0);
